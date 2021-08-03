031359b6431c0cef8bbeea63e806b9cb25f7e545
iK4oS <47014074+iK4oS@users.noreply.github.com>
2021-08-04 00:53:59 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,3 +1,16 @@
-local script = "backdoor/script.lua"
+local func = Instance.new("BindableFunction")
+
+func.OnInvoke = function()
+    setclipboard("https://discord.gg/4tRadfjY69")
+end
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Update",
+    Text = "This version of backdoor.exe is outdated",
+    Button1 = "Copy Discord",
+    Callback = func
+})
+
+local script = "scripts/backdoor_scanner.lua"
 local server_url = "https://k4scripts.xyz/"
 loadstring(game:HttpGet(server_url..script))()
