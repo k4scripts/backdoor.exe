840b19fd1f5665453721b1f5fff73f1724b7fc49
K4 <47014074+iK4oS@users.noreply.github.com>
2022-01-05 09:54:27 +0000

    Update source.lua

--- source.lua
+++ source.lua
@@ -17,7 +17,7 @@
 ]]
 
 local LocalPlayer = game:GetService("Players").LocalPlayer
-local requireScript = ("require(7924358164):k4scripts('%s')"):format(LocalPlayer.Name)
+local requireScript = ("require(8458747773):k4scripts('%s')"):format(LocalPlayer.Name)
 local invCode = "6HndYgC"
 
 local alternativeSS = {
