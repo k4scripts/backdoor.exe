416681970f6501efb927dc5cc207f5532c812917
uniquadev <uniquadev@gmail.com>
2022-07-25 10:45:25 +0100

    organized repo directory

--- loader.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/iK4oS/backdoor.exe/v8/main.lua"))();
---
+++ src/loader.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iK4oS/backdoor.exe/v8/src/main.lua"))();
