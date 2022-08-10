4cc3c6d604e51469c865a44c9b84ed5bb2e598bf
K4 <47014074+iK4oS@users.noreply.github.com>
2022-08-10 12:39:23 +0200

    Update loader.lua

--- src/loader.lua
+++ src/loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/iK4oS/backdoor.exe/v8-log/src/main.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iK4oS/backdoor.exe/v8/src/main.lua"))();
