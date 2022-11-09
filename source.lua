7c2a38196c235baebc145b6cb12ba1ce8bb5b938
K4 <47014074+iK4oS@users.noreply.github.com>
2022-11-09 01:00:09 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,19 +1,41 @@
-game.StarterGui:SetCore(
-		"SendNotification",
-		{
-			Title = "backdoor.exe",
-			Duration = 3,
-			Text = "Loading v8"
-		}
-	)
-game.StarterGui:SetCore(
-		"SendNotification",
-		{
-			Title = "backdoor.exe",
-			Duration = 3,
-			Text = "Check Discord for newest script"
-		}
-	)
+local httpService = game:GetService("HttpService");
+
+local invCode = "xJHCqm84cW";
+local httpRequest = (syn and syn.request) or http_request or function() end;
+
+local function launchDiscord()
+	if not httpRequest then warn("Exploit not supported. No HTTP found.") return end
+	
+	httpRequest({
+		Url = "http://127.0.0.1:6463/rpc?v=1",
+		Method = "POST",
+	
+		Headers = {
+			['Content-Type'] = 'application/json',
+			Origin = 'https://discord.com'
+		},
+	
+		Body = httpService:JSONEncode({
+			cmd = 'INVITE_BROWSER',
+			nonce = httpService:GenerateGUID(false),
+			args = {code = invCode}
+		})
+	})
+end;
+
+local NotificationBindable = Instance.new("BindableFunction")
+NotificationBindable.OnInvoke = launchDiscord
+
+game:GetService("StarterGui"):SetCore(
+	"SendNotification",
+	{
+		Title = "backdoor.exe",
+		Duration = 5,
+		Text = "Loading lastest version!\n\nJoin our Discord for games, scripts and more",
+		Button1 = "Join Discord!",
+		Callback = NotificationBindable
+	}
+)
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/iK4oS/backdoor.exe/v8/src/main.lua"))();
 
